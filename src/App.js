e8c1902968c7c5835b1f9c54a1523b657b0634b6
vijaylaxmi hanamshetty <vijaylaxmihanamshetty38@.in>
2024-09-14 05:09:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './layout/Sidebar/Sidebar';
+import Content from './layout/Content/Content';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='app'>
+        <Sidebar />
+        <Content />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
